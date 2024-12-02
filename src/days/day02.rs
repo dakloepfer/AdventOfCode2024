72175b67cbc55d95ad7df01496533ed59407b6fc
Dominik Kloepfer <dominik.kloepfer@helsing.ai>
2024-12-02 12:42:41 +0100

    Task 1 Day 02

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,4 +1,6 @@
+use std::fs;
 use std::io::Error;
+use std::io::Write;
 
 pub fn run() -> Result<(), Error> {
     let _ = task1();
@@ -11,6 +13,60 @@ pub fn run() -> Result<(), Error> {
 
 fn task1() -> Result<(), Error> {
     println!("Computing solution for task 1 of Day 2...");
+
+    let input_data = fs::read_to_string(
+        "/Users/dominik.kloepfer/Documents/advent-of-code-2024/input_data/day02_input.txt",
+    )?;
+
+    let mut num_safe: u32 = 0;
+    for report in input_data.lines() {
+        let levels: Vec<i32> = report
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+
+        if levels.len() < 2 {
+            num_safe += 1;
+            continue;
+        }
+
+        let mut safe = true;
+        let increasing = levels[1] - levels[0] > 0;
+        let mut prev_level = levels[0];
+        for level in levels.iter().skip(1) {
+            let level_diff = level - prev_level;
+
+            if increasing {
+                if level_diff <= 0 {
+                    safe = false;
+                    break;
+                }
+            } else {
+                // decreasing
+                if level_diff >= 0 {
+                    safe = false;
+                    break;
+                }
+            }
+            if level_diff.abs() > 3 {
+                safe = false;
+                break;
+            }
+            prev_level = *level;
+        }
+        if safe {
+            num_safe += 1;
+        }
+    }
+
+    let mut solution_file = fs::File::create("solutions/day02_solution.txt")?;
+    writeln!(solution_file, "Solution for Task 1 of Day 02:")?;
+    writeln!(
+        solution_file,
+        "The total number of safe reports is {}.",
+        num_safe
+    )?;
+
     Ok(())
 }
 fn task2() -> Result<(), Error> {
