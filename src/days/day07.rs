0ac722af62754ac28d527d1ed368b9baab89802f
Dominik Kloepfer <dominik.kloepfer@helsing.ai>
2024-12-07 17:32:39 +0100

    Day 07 Task 1

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering::{Equal, Greater, Less};
 use std::fs;
 use std::io::Error;
 use std::io::Write;
@@ -16,11 +17,69 @@ fn task1() -> Result<(), Error> {
 
     let input_data = fs::read_to_string("input_data/day07_input.txt")?;
 
-    let solution = 0;
+    let mut sum_of_valid_test_values: u64 = 0;
+    for line in input_data.lines() {
+        let initial_parts: Vec<&str> = line.split(':').collect();
+        let test_value = initial_parts
+            .first()
+            .expect("Line is empty?")
+            .parse::<u64>()
+            .expect("could not parse test value.");
 
-    let mut solution_file = fs::File::create("solutions/day04_solution.txt")?;
+        let numbers: Vec<u64>;
+        if let Some(second_element) = initial_parts.get(1) {
+            numbers = second_element
+                .split_whitespace()
+                .filter_map(|s| s.parse::<u64>().ok())
+                .collect();
+        } else {
+            numbers = Vec::new();
+            eprintln!("The line does not have any numbers.");
+        }
+
+        let mut valid = false;
+        let mut stack: Vec<(usize, u64)> = vec![(1, *numbers.first().expect("numbers is empty!"))];
+
+        while let Some((current_idx, current_result)) = stack.pop() {
+            if let Some(current_number) = numbers.get(current_idx) {
+                let result_add_op = current_result + current_number;
+                match result_add_op.cmp(&test_value) {
+                    Equal => {
+                        valid = true;
+                        break;
+                    }
+                    Less => {
+                        stack.push((current_idx + 1, result_add_op));
+                    }
+                    Greater => {}
+                }
+
+                let result_mul_op = current_result * current_number;
+                match result_mul_op.cmp(&test_value) {
+                    Equal => {
+                        valid = true;
+                        break;
+                    }
+                    Less => {
+                        stack.push((current_idx + 1, result_mul_op));
+                    }
+                    Greater => {}
+                }
+            }
+        }
+
+        if valid {
+            sum_of_valid_test_values += test_value;
+        }
+    }
+
+    let mut solution_file = fs::File::create("solutions/day07_solution.txt")?;
     writeln!(solution_file, "Solution for Task 1 of Day 07:")?;
-    writeln!(solution_file, "TODO {}.", solution)?;
+    writeln!(
+        solution_file,
+        "The sum of the test values of equations that could be true is {}.",
+        sum_of_valid_test_values
+    )?;
 
     Ok(())
 }
