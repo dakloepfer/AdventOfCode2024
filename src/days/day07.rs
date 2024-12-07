11c6fbbb5765d777a6f086e45abb4c7c2768a4cf
Dominik Kloepfer <dominik.kloepfer@helsing.ai>
2024-12-07 17:57:25 +0100

    refactoring

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,7 +1,7 @@
 use std::cmp::Ordering::{Equal, Greater, Less};
-use std::fs;
 use std::io::Error;
 use std::io::Write;
+use std::{fs, io};
 
 pub fn run() -> Result<(), Error> {
     let _ = task1();
@@ -10,6 +10,46 @@ pub fn run() -> Result<(), Error> {
     println!("Completed solutions for Day 7!");
 
     Ok(())
+}
+
+fn check_equation(
+    output_value: u64,
+    numbers: Vec<u64>,
+    operators: Vec<&str>,
+) -> Result<bool, Error> {
+    let mut valid = false;
+    let mut stack: Vec<(usize, u64)> = vec![(1, *numbers.first().expect("numbers is empty!"))];
+
+    while let Some((current_idx, current_result)) = stack.pop() {
+        if let Some(current_number) = numbers.get(current_idx) {
+            for operator in operators.iter() {
+                let result: u64;
+                if operator == &"add" {
+                    result = current_result + current_number;
+                } else if operator == &"mul" {
+                    result = current_result * current_number;
+                } else {
+                    eprintln!("Unknown operator {}!", operator);
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "Unknown Operator",
+                    ));
+                }
+                match result.cmp(&output_value) {
+                    Equal => {
+                        valid = true;
+                        break;
+                    }
+                    Less => {
+                        stack.push((current_idx + 1, result));
+                    }
+                    Greater => {}
+                }
+            }
+        }
+    }
+
+    Ok(valid)
 }
 
 fn task1() -> Result<(), Error> {
@@ -37,36 +77,7 @@ fn task1() -> Result<(), Error> {
             eprintln!("The line does not have any numbers.");
         }
 
-        let mut valid = false;
-        let mut stack: Vec<(usize, u64)> = vec![(1, *numbers.first().expect("numbers is empty!"))];
-
-        while let Some((current_idx, current_result)) = stack.pop() {
-            if let Some(current_number) = numbers.get(current_idx) {
-                let result_add_op = current_result + current_number;
-                match result_add_op.cmp(&test_value) {
-                    Equal => {
-                        valid = true;
-                        break;
-                    }
-                    Less => {
-                        stack.push((current_idx + 1, result_add_op));
-                    }
-                    Greater => {}
-                }
-
-                let result_mul_op = current_result * current_number;
-                match result_mul_op.cmp(&test_value) {
-                    Equal => {
-                        valid = true;
-                        break;
-                    }
-                    Less => {
-                        stack.push((current_idx + 1, result_mul_op));
-                    }
-                    Greater => {}
-                }
-            }
-        }
+        let valid = check_equation(test_value, numbers, vec!["add", "mul"])?;
 
         if valid {
             sum_of_valid_test_values += test_value;
@@ -77,7 +88,7 @@ fn task1() -> Result<(), Error> {
     writeln!(solution_file, "Solution for Task 1 of Day 07:")?;
     writeln!(
         solution_file,
-        "The sum of the test values of equations that could be true is {}.",
+        "The sum of the test values of equations that could be true with addition and multiplication is {}.",
         sum_of_valid_test_values
     )?;
 
