7d72f2c90e96bf8299adfd8bc6df2e067a96bdf1
Dominik Kloepfer <dominik.kloepfer@helsing.ai>
2024-12-17 12:55:22 +0100

    This would work in theory (given enough computation, assuming no overflow for the u32), but takes too long. Actual solution is obtained by inspecting the provided input.

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -13,12 +13,13 @@ fn program_from_str(input: &str) -> Vec<u32> {
     program_str.split(',').flat_map(|val| val.parse()).collect()
 }
 
+#[derive(Copy, Clone)]
 struct Computer {
     // the instructions say that registers can be any integer, but if the initial values are all >=0, then they will stay so.
     // so since it makes other things easier (especially the 2^reg_a operation in dv and the XOR), I'll keep them as unsigned ints.
-    reg_a: u32,
-    reg_b: u32,
-    reg_c: u32,
+    reg_a: u64,
+    reg_b: u64,
+    reg_c: u64,
     instruction_pointer: u32,
 }
 impl Computer {
@@ -29,7 +30,7 @@ impl Computer {
 
         for line in config.trim().lines() {
             let (_, reg_val_str) = line.trim().split_once(": ").unwrap();
-            let reg_val: u32 = reg_val_str.parse().unwrap();
+            let reg_val: u64 = reg_val_str.parse().unwrap();
 
             if line.contains("A") {
                 reg_a = reg_val;
@@ -47,7 +48,7 @@ impl Computer {
             instruction_pointer: 0,
         }
     }
-    fn operand_to_combo(&self, operand: u32) -> u32 {
+    fn operand_to_combo(&self, operand: u32) -> u64 {
         match operand {
             0 => 0,
             1 => 1,
@@ -61,9 +62,9 @@ impl Computer {
             }
         }
     }
-    fn compute_dv(&mut self, operand: u32) -> u32 {
+    fn compute_dv(&mut self, operand: u32) -> u64 {
         let numerator = self.reg_a;
-        let denominator = 2u32.pow(self.operand_to_combo(operand));
+        let denominator = 2u64.pow(self.operand_to_combo(operand) as u32);
 
         numerator / denominator
     }
@@ -71,7 +72,7 @@ impl Computer {
         self.reg_a = self.compute_dv(operand);
     }
     fn exec_bxl(&mut self, operand: u32) {
-        self.reg_b ^= operand;
+        self.reg_b ^= operand as u64;
     }
     fn exec_bst(&mut self, operand: u32) {
         self.reg_b = self.operand_to_combo(operand) % 8;
@@ -89,7 +90,7 @@ impl Computer {
     fn exec_bxc(&mut self, _operand: u32) {
         self.reg_b ^= self.reg_c;
     }
-    fn exec_out(&mut self, operand: u32) -> u32 {
+    fn exec_out(&mut self, operand: u32) -> u64 {
         self.operand_to_combo(operand) % 8
     }
     fn exec_bdv(&mut self, operand: u32) {
@@ -99,33 +100,60 @@ impl Computer {
         self.reg_c = self.compute_dv(operand);
     }
 
-    fn execute_program(&mut self, program: Vec<u32>) -> Vec<u32> {
-        let mut output: Vec<u32> = Vec::new();
+    /// return whether we jumped
+    fn execute_instruction(&mut self, instruction: u32, operand: u32) -> (bool, Option<u64>) {
+        match instruction {
+            0 => self.exec_adv(operand),
+            1 => self.exec_bxl(operand),
+            2 => self.exec_bst(operand),
+            3 => return (self.exec_jnz(operand), None),
+            4 => self.exec_bxc(operand),
+            5 => return (false, Some(self.exec_out(operand))),
+            6 => self.exec_bdv(operand),
+            7 => self.exec_cdv(operand),
+            _ => panic!("Invalid instruction: {}", instruction),
+        }
+        (false, None)
+    }
+    fn execute_program(&mut self, program: Vec<u32>) -> Vec<u64> {
+        let mut output: Vec<u64> = Vec::new();
 
         while self.instruction_pointer < program.len() as u32 - 1 {
             let instruction = program[self.instruction_pointer as usize];
             let operand = program[self.instruction_pointer as usize + 1];
 
-            match instruction {
-                0 => self.exec_adv(operand),
-                1 => self.exec_bxl(operand),
-                2 => self.exec_bst(operand),
-                3 => {
-                    let jumped = self.exec_jnz(operand);
-                    if jumped {
-                        continue;
-                    }
-                }
-                4 => self.exec_bxc(operand),
-                5 => output.push(self.exec_out(operand)),
-                6 => self.exec_bdv(operand),
-                7 => self.exec_cdv(operand),
-                _ => panic!("Invalid instruction: {}", instruction),
+            let (jumped, output_maybe) = self.execute_instruction(instruction, operand);
+            if let Some(out_val) = output_maybe {
+                output.push(out_val);
             }
-            self.instruction_pointer += 2;
+            if !jumped {
+                self.instruction_pointer += 2;
+            }
         }
 
         output
+    }
+
+    fn check_output_program_copy(&mut self, program: Vec<u32>) -> bool {
+        let mut output: Vec<u64> = Vec::new();
+
+        while self.instruction_pointer < program.len() as u32 - 1 {
+            let instruction = program[self.instruction_pointer as usize];
+            let operand = program[self.instruction_pointer as usize + 1];
+
+            let (jumped, output_maybe) = self.execute_instruction(instruction, operand);
+            if let Some(out_val) = output_maybe {
+                if output.len() == program.len() || out_val != program[output.len()] as u64 {
+                    return false;
+                }
+                output.push(out_val);
+            }
+            if !jumped {
+                self.instruction_pointer += 2;
+            }
+        }
+
+        output.len() == program.len()
     }
 }
 
@@ -168,8 +196,24 @@ fn task1() -> Result<(), Error> {
 
 fn task2() -> Result<(), Error> {
     println!("Computing solution for task 2 of Day 17...");
+    println!("This would work in theory (until overflow), but this takes way too long. The actual solution needs to be obtained by analysing the program's actions and back-tracking the necessary register states.");
 
-    let solution = 0; // TODO
+    let input_data = fs::read_to_string("input_data/day17_input.txt")?;
+
+    let (computer_config, program_str) = input_data.split_once("\n\n").unwrap();
+    let program = program_from_str(program_str);
+    let computer = Computer::from_config(computer_config);
+
+    let mut reg_a_start_value = 0;
+    loop {
+        let mut test_computer = computer;
+        test_computer.reg_a = reg_a_start_value;
+
+        if test_computer.check_output_program_copy(program.clone()) {
+            break;
+        }
+        reg_a_start_value += 1;
+    }
 
     let mut solution_file = fs::OpenOptions::new()
         .append(true)
@@ -177,7 +221,7 @@ fn task2() -> Result<(), Error> {
         .open("solutions/day17_solution.txt")?;
     writeln!(solution_file)?;
     writeln!(solution_file, "Solution for Task 2 of Day 17:")?;
-    writeln!(solution_file, "TODO {}.", solution)?;
+    writeln!(solution_file, "The smallest initial value of register A that causes the program to output a copy of itself is {}.", reg_a_start_value)?;
 
     Ok(())
 }
