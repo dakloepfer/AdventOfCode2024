84295d489d4323ee4d8ba7835c6503992a63cc94
Dominik Kloepfer <dominik.kloepfer@helsing.ai>
2024-12-26 01:17:57 +0100

    Day 25 Task 2

--- src/days/day25.rs
+++ src/days/day25.rs
@@ -102,15 +102,16 @@ fn task1() -> Result<(), Error> {
 fn task2() -> Result<(), Error> {
     println!("Computing solution for task 2 of Day 25...");
 
-    let solution = 0; // TODO
-
     let mut solution_file = fs::OpenOptions::new()
         .append(true)
         .create(true)
         .open("solutions/day25_solution.txt")?;
     writeln!(solution_file)?;
     writeln!(solution_file, "Solution for Task 2 of Day 25:")?;
-    writeln!(solution_file, "TODO {}.", solution)?;
+    writeln!(
+        solution_file,
+        "The chronicle has been successfully completed."
+    )?;
 
     Ok(())
 }
